9d321b1eceaff2e09d6cfc3fa68254b47abaedd1
Vladislav <102545184+lezver@users.noreply.github.com>
2023-07-07 19:45:23 +0300

    fixed error 404

--- src/components/AboutMovie/AboutMovie.jsx
+++ src/components/AboutMovie/AboutMovie.jsx
@@ -1,46 +1,41 @@
 import './AboutMovie.scss';
 import PropTypes from 'prop-types';
 
-export const AboutMovie = ({
-  data: {
-    title,
-    original_title,
-    vote_average,
-    overview,
-    genres,
-    backdrop_path,
-  },
-}) => (
+export const AboutMovie = ({ data }) => (
   <div className="about-movie">
     <img
       src={
-        backdrop_path
-          ? `https://image.tmdb.org/t/p/w400${backdrop_path}`
+        data?.backdrop_path
+          ? `https://image.tmdb.org/t/p/w400${data?.backdrop_path}`
           : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmVm-NOFWAwzSDCG2503S24gnb6ij0l6Qz1URGonjsEqkf6fmGza-C7SW9iuHQaJj_7sA&usqp=CAU'
       }
-      alt={title}
+      alt={data?.title ?? 'no name'}
     />
     <ul>
       <li>
-        <h3>{title && original_title}</h3>
+        <h3>{data?.title ?? data?.original_title ?? 'No name'}</h3>
       </li>
       <li>
-        <span>User score: {vote_average?.toString().slice(0, 3)}</span>
+        <span>
+          User score: {data?.vote_average?.toString().slice(0, 3) ?? 0}
+        </span>
       </li>
       <li>
         <h4>Overview</h4>
       </li>
       <li>
-        <span>{overview}</span>
+        <span>{data?.overview ?? 'Coming soon.'}</span>
       </li>
       <li>
         <h5>Genres</h5>
       </li>
       <li>
         <ul>
-          {genres?.map(({ id, name }) => (
-            <li key={id}>{name}</li>
-          ))}
+          {data?.genres?.map(({ id, name }) => <li key={id}>{name}</li>) ?? (
+            <li>
+              <span>Coming soon.</span>
+            </li>
+          )}
         </ul>
       </li>
     </ul>
